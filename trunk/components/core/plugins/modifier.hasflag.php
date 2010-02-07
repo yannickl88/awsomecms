a040240d7e922b8a20815556adf37e36e9fa77d1
yannick.l.88 <yannick.l.88@c9b2d25c-0a91-11df-a582-91aacf084772>
2010-02-07 21:14:45 +0000

    added even more documentation.

--- trunk/components/core/plugins/modifier.hasflag.php
+++ trunk/components/core/plugins/modifier.hasflag.php
@@ -1,5 +1,19 @@
 <?php
 /**
+ * This file is part of the A.W.S.O.M.E.cms distribution.
+ * Detailed copyright and licensing information can be found
+ * in the doc/COPYRIGHT and doc/LICENSE files which should be
+ * included in the distribution.
+ *
+ * @package components.core.plugins
+ *
+ * @copyright (c) 2009-2010 Yannick de Lange
+ * @license http://www.gnu.org/licenses/gpl.txt
+ *
+ * @version $Revision$
+ */
+
+/**
 * Modifier to binary check if a flag is in a value
 * 
 * @param Int $int
--- trunk/components/news/plugins/function.news.php
+++ trunk/components/news/plugins/function.news.php
@@ -1,4 +1,25 @@
 <?php
+/**
+ * This file is part of the A.W.S.O.M.E.cms distribution.
+ * Detailed copyright and licensing information can be found
+ * in the doc/COPYRIGHT and doc/LICENSE files which should be
+ * included in the distribution.
+ *
+ * @package components.news.plugins
+ *
+ * @copyright (c) 2009-2010 Yannick de Lange
+ * @license http://www.gnu.org/licenses/gpl.txt
+ *
+ * @version $Revision$
+ */
+
+/**
+ * Show a list of news items
+ * 
+ * @param array $params
+ * @param Smarty $smarty
+ * @return string
+ */
 function smarty_function_news($params, &$smarty)
 {
     $newsComponent = Table::init('news.news');
