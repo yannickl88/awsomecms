2877e777951f043a3cc75ab695f0898b4b99a543
yannick.l.88 <yannick.l.88@c9b2d25c-0a91-11df-a582-91aacf084772>
2010-02-16 23:01:15 +0000

    * Validation is now done with the db value rather then the request value * Added documentation to components that were missing it * Added dutch language files * Removed skimfo component

--- trunk/components/twitter/plugins/function.twitter.php
+++ trunk/components/twitter/plugins/function.twitter.php
@@ -1,4 +1,25 @@
 <?php
+/**
+ * This file is part of the A.W.S.O.M.E.cms distribution.
+ * Detailed copyright and licensing information can be found
+ * in the doc/COPYRIGHT and doc/LICENSE files which should be
+ * included in the distribution.
+ *
+ * @package components.twitter.plugins
+ *
+ * @copyright (c) 2009-2010 Yannick de Lange
+ * @license http://www.gnu.org/licenses/gpl.txt
+ *
+ * @version $Revision$
+ */
+
+/**
+ * Add the latest twitter message to your site
+ * 
+ * @param array $params
+ * @param Smarty $smarty
+ * @return string
+ */
 function smarty_function_twitter($params, &$smarty)
 {
     include_once Config::get('websiteroot', '.').'/../components/twitter/util/class.Twitter.inc';
