a4066a74ba828c823585b8c54f26cd9ebb74a357
yannick.l.88 <yannick.l.88@c9b2d25c-0a91-11df-a582-91aacf084772>
2010-02-16 23:03:21 +0000

    * fixed a page that had the wrong form

--- components/slideshow/db/admin.sql
+++ components/slideshow/db/admin.sql
@@ -1,8 +1,8 @@
-INSERT INTO `pages` (`page_name`, `page_location`, `page_template`, `page_timestamp`) VALUES('add', '/admin/slideshow/image/', '{form component=''slideshow'' form=''add_image''}', NOW());
 INSERT INTO `pages` (`page_name`, `page_location`, `page_template`, `page_timestamp`) VALUES('admin', '/admin/slideshow/group/', '{form table=''slideshow.groups'' form=''admin''}', NOW());
+INSERT INTO `pages` (`page_name`, `page_location`, `page_template`, `page_timestamp`) VALUES('admin', '/admin/slideshow/image/', '{form table=''slideshow.images'' form=''admin''}', NOW());
+INSERT INTO `pages` (`page_name`, `page_location`, `page_template`, `page_timestamp`) VALUES('add', '/admin/slideshow/image/', '{form component=''slideshow'' form=''add_image''}', NOW());
 INSERT INTO `pages` (`page_name`, `page_location`, `page_template`, `page_timestamp`) VALUES('add', '/admin/slideshow/group/', '{form component=''slideshow'' form=''add_group''}', NOW());
 INSERT INTO `pages` (`page_name`, `page_location`, `page_template`, `page_timestamp`) VALUES('edit', '/admin/slideshow/group/', '{form component=''slideshow'' form=''edit_group''}', NOW());
 INSERT INTO `pages` (`page_name`, `page_location`, `page_template`, `page_timestamp`) VALUES('edit', '/admin/slideshow/image/', '{form component=''slideshow'' form=''edit_image''}', NOW());
-INSERT INTO `pages` (`page_name`, `page_location`, `page_template`, `page_timestamp`) VALUES('delete', '/admin/slideshow/group/', '{form component=''slideshow'' form=''delete_group''}', NOW());
+INSERT INTO `pages` (`page_name`, `page_location`, `page_template`, `page_timestamp`) VALUES('delete', '/admin/slideshow/group/', '{form table=''slideshow.groups'' form=''delete_group''}', NOW());
 INSERT INTO `pages` (`page_name`, `page_location`, `page_template`, `page_timestamp`) VALUES('delete', '/admin/slideshow/image/', '{form table=''slideshow.images'' form=''delete''}', NOW());
-INSERT INTO `pages` (`page_name`, `page_location`, `page_template`, `page_timestamp`) VALUES('admin', '/admin/slideshow/image/', '{form table=''slideshow.images'' form=''admin''}', NOW());
