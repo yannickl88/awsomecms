c3582459f153f6567a7339286e6ffa457b71b2ef
yannick.l.88 <yannick.l.88@c9b2d25c-0a91-11df-a582-91aacf084772>
2010-02-17 21:09:40 +0000

    fixed page content

--- trunk/components/slideshow/db/admin.sql
+++ trunk/components/slideshow/db/admin.sql
@@ -4,5 +4,5 @@ INSERT INTO `pages` (`page_name`, `page_location`, `page_template`, `page_timest
 INSERT INTO `pages` (`page_name`, `page_location`, `page_template`, `page_timestamp`) VALUES('add', '/admin/slideshow/group/', '{form component=''slideshow'' form=''add_group''}', NOW());
 INSERT INTO `pages` (`page_name`, `page_location`, `page_template`, `page_timestamp`) VALUES('edit', '/admin/slideshow/group/', '{form component=''slideshow'' form=''edit_group''}', NOW());
 INSERT INTO `pages` (`page_name`, `page_location`, `page_template`, `page_timestamp`) VALUES('edit', '/admin/slideshow/image/', '{form component=''slideshow'' form=''edit_image''}', NOW());
-INSERT INTO `pages` (`page_name`, `page_location`, `page_template`, `page_timestamp`) VALUES('delete', '/admin/slideshow/group/', '{form table=''slideshow.groups'' form=''delete_group''}', NOW());
+INSERT INTO `pages` (`page_name`, `page_location`, `page_template`, `page_timestamp`) VALUES('delete', '/admin/slideshow/group/', '{form table=''slideshow.groups'' form=''delete''}', NOW());
 INSERT INTO `pages` (`page_name`, `page_location`, `page_template`, `page_timestamp`) VALUES('delete', '/admin/slideshow/image/', '{form table=''slideshow.images'' form=''delete''}', NOW());
