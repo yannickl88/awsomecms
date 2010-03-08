40c54d20614883038460abb96dfcf4598c50fb67
yannick.l.88 <yannick.l.88@c9b2d25c-0a91-11df-a582-91aacf084772>
2010-03-08 23:32:37 +0000

    * added the new tree.js * added missing / to the location

--- trunk/components/page/install/js/tree.js
+++ trunk/components/page/install/js/tree.js
@@ -1,20 +1,44 @@
-function admin_toggleTree(id)
-{
-    $('.link'+id).toggleClass('open');
-    $('.sub'+id).toggleClass('hidden');
-    
-    if(admin_hasCookie('menu'+id))
-    {
-        admin_eraseCookie('menu'+id);
-    }
-    else
-    {
-        admin_createCookie('menu'+id, 1);
-    }
-    
-    return false;
-}
+/**
+ * This file is part of the A.W.S.O.M.E.cms distribution.
+ * Detailed copyright and licensing information can be found
+ * in the doc/COPYRIGHT and doc/LICENSE files which should be
+ * included in the distribution.
+ *
+ * @package js
+ *
+ * @copyright (c) 2009-2010 Yannick de Lange
+ * @license http://www.gnu.org/licenses/gpl.txt
+ *
+ * @version $Revision$
+ */
 
+var admin_treeTarget = "";
+/**
+ * Check the classes from an element that contain an ID
+ * 
+ * @param Array array
+ * @return String		or False on failure
+ */
+function admin_fetchIDFromClasses(array)
+{
+	for(var key in array)
+	{
+		var value = array[key];
+		if(value.substr(0, 4) === "link")
+		{
+			return value.substr(4);
+		}
+	}
+	return false;
+}
+/**
+ * Create a Cookie
+ * 
+ * @param String name
+ * @param String value
+ * @param Date days
+ * @return Boolean		False
+ */
 function admin_createCookie(name,value,days) {
     if (days) 
     {
@@ -29,7 +53,12 @@ function admin_createCookie(name,value,days) {
     
     return false;
 }
-
+/**
+ * Read a Cookie
+ * 
+ * @param String name
+ * @return String 		or False on failure
+ */
 function admin_readCookie(name) {
     var nameEQ = name + "=";
     var ca = document.cookie.split(';');
@@ -45,13 +74,85 @@ function admin_readCookie(name) {
     }
     return false;
 }
-
+/**
+ * Remove a cookie
+ * 
+ * @param String name
+ * @return Boolean		False
+ */
 function admin_eraseCookie(name) {
     admin_createCookie(name,"",-1);
     
     return false;
 }
-
+/**
+ * Check if there is a cookie
+ * 
+ * @param String name
+ * @return Boolean
+ */
 function admin_hasCookie(name) {
     return (admin_readCookie(name));
 }
+/**
+ * Mouse over listener
+ * 
+ * @param Event event
+ */
+function admin_mouseOver(event)
+{
+	$(this).addClass('focus')
+}
+/**
+ * Mouse out listener
+ * 
+ * @param Event event
+ */
+function admin_mouseOut(event)
+{
+	$(this).removeClass('focus')
+}
+/**
+ * Mouse click listener
+ * 
+ * @param Event event
+ * @return Boolean		False
+ */
+function admin_mouseClick(event)
+{
+	var element = $(this);
+	
+	if($(this).hasClass("clickableTreeItem") && $(this).hasClass("javascript"))
+	{
+		$("#"+admin_treeTarget).val($(this).attr("href"));
+		return false;
+	}
+	else if($(this).hasClass("clickableTreeItem"))
+	{
+		element = $(this).siblings(".clickableTreeArrow");
+	}
+	
+	element.toggleClass('open');
+	element.siblings(".treeNode").toggleClass('hidden');
+	
+	var id = admin_fetchIDFromClasses(element.attr("class").split(" "))
+	
+    if(admin_hasCookie('menu'+id))
+    {
+        admin_eraseCookie('menu'+id);
+    }
+    else
+    {
+        admin_createCookie('menu'+id, 1);
+    }
+	
+    return false;
+}
+
+//some listeners for the clickeable items
+$(document).ready(function(e) {
+	$(".clickableTreeArrow").mouseover(admin_mouseOver);
+	$(".clickableTreeArrow").mouseout(admin_mouseOut);
+	$(".clickableTreeArrow").click(admin_mouseClick);
+	$(".clickableTreeItem").click(admin_mouseClick);
+});
