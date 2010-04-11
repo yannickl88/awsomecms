2fc946280236121db68255f63acb4e95a1cd1039
yannick.l.88 <yannick.l.88@c9b2d25c-0a91-11df-a582-91aacf084772>
2010-04-11 16:38:04 +0000

    * Started working on HTML5 Upload for drag and drop support

--- trunk/components/page/install/js/tree.js
+++ trunk/components/page/install/js/tree.js
@@ -21,15 +21,15 @@ var admin_treeTarget = "";
  */
 function admin_fetchIDFromClasses(array)
 {
-	for(var key in array)
-	{
-		var value = array[key];
-		if(value.substr(0, 4) === "link")
-		{
-			return value.substr(4);
-		}
-	}
-	return false;
+    for(var key in array)
+    {
+        var value = array[key];
+        if(value.substr(0, 4) === "link")
+        {
+            return value.substr(4);
+        }
+    }
+    return false;
 }
 /**
  * Create a Cookie
@@ -48,9 +48,9 @@ function admin_createCookie(name,value,days) {
     }
     else 
         var expires = "";
-    
+
     document.cookie = name+"="+value+expires+"; path=/";
-    
+
     return false;
 }
 /**
@@ -65,10 +65,10 @@ function admin_readCookie(name) {
     for(var i=0;i < ca.length;i++) 
     {
         var c = ca[i];
-        
+
         while (c.charAt(0)==' ') 
             c = c.substring(1,c.length);
-        
+
         if (c.indexOf(nameEQ) == 0) 
             return c.substring(nameEQ.length,c.length);
     }
@@ -82,7 +82,7 @@ function admin_readCookie(name) {
  */
 function admin_eraseCookie(name) {
     admin_createCookie(name,"",-1);
-    
+
     return false;
 }
 /**
@@ -101,7 +101,7 @@ function admin_hasCookie(name) {
  */
 function admin_mouseOver(event)
 {
-	$(this).addClass('focus')
+    $(this).addClass('focus')
 }
 /**
  * Mouse out listener
@@ -110,7 +110,7 @@ function admin_mouseOver(event)
  */
 function admin_mouseOut(event)
 {
-	$(this).removeClass('focus')
+    $(this).removeClass('focus')
 }
 /**
  * Mouse click listener
@@ -120,23 +120,23 @@ function admin_mouseOut(event)
  */
 function admin_mouseClick(event)
 {
-	var element = $(this);
-	
-	if($(this).hasClass("clickableTreeItem") && $(this).hasClass("javascript"))
-	{
-		$("#"+admin_treeTarget).val($(this).attr("href"));
-		return false;
-	}
-	else if($(this).hasClass("clickableTreeItem"))
-	{
-		element = $(this).siblings(".clickableTreeArrow");
-	}
-	
-	element.toggleClass('open');
-	element.siblings(".treeNode").toggleClass('hidden');
-	
-	var id = admin_fetchIDFromClasses(element.attr("class").split(" "))
-	
+    var element = $(this);
+
+    if($(this).hasClass("clickableTreeItem") && $(this).hasClass("javascript"))
+    {
+        $("#"+admin_treeTarget).val($(this).attr("href"));
+        return false;
+    }
+    else if($(this).hasClass("clickableTreeItem"))
+    {
+        element = $(this).siblings(".clickableTreeArrow");
+    }
+
+    element.toggleClass('open');
+    element.siblings(".treeNode").toggleClass('hidden');
+
+    var id = admin_fetchIDFromClasses(element.attr("class").split(" "))
+
     if(admin_hasCookie('menu'+id))
     {
         admin_eraseCookie('menu'+id);
@@ -145,14 +145,102 @@ function admin_mouseClick(event)
     {
         admin_createCookie('menu'+id, 1);
     }
-	
+
     return false;
+}
+
+function admin_upload(event){
+    var data = event.dataTransfer;
+    if(data && data.files.length > 0)
+    {
+        var boundary = '------multipartformboundary' + (new Date).getTime();
+        var dashdash = '--';
+        var crlf     = '\r\n';
+    
+        /* Build RFC2388 string. */
+        var builder = '';
+    
+        builder += dashdash;
+        builder += boundary;
+        builder += crlf;
+        
+        var xhr = new XMLHttpRequest();
+        
+        /* For each dropped file. */
+        for (var i = 0; i < data.files.length; i++) {
+            var file = data.files[i];
+    
+            /* Generate headers. */            
+            builder += 'Content-Disposition: form-data; name="user_file[]"';
+            if (file.fileName) {
+              builder += '; filename="' + file.fileName + '"';
+            }
+            builder += crlf;
+    
+            builder += 'Content-Type: application/octet-stream';
+            builder += crlf;
+            builder += crlf; 
+    
+            /* Append binary data. */
+            builder += file.getAsBinary();
+            builder += crlf;
+    
+            /* Write boundary. */
+            builder += dashdash;
+            builder += boundary;
+            builder += crlf;
+        }
+        
+        /* Mark end of the request. */
+        builder += dashdash;
+        builder += boundary;
+        builder += dashdash;
+        builder += crlf;
+    
+        xhr.open("POST", "/", true);
+        xhr.setRequestHeader('content-type', 'multipart/form-data; boundary=' 
+            + boundary);
+        xhr.sendAsBinary(builder);
+        
+        xhr.onload = function(event) { 
+            /* If we got an error display it. */
+            if (xhr.responseText) {
+                alert(xhr.responseText);
+            }
+            $("#dropzone").load("list.php?random=" +  (new Date).getTime());
+        };
+    }
+    
+    /* Prevent FireFox opening the dragged file. */
+    event.stopPropagation();
+}
+function admin_dragenter(e) {
+    $(".contentroot").addClass("fileOver");
+    e.stopPropagation();
+    e.preventDefault();
+}
+function admin_dragover(e) {
+    
+    e.stopPropagation();
+    e.preventDefault();
+}
+function admin_dragout(e) {
+    $(".contentroot").removeClass("fileOver");
 }
 
 //some listeners for the clickeable items
 $(document).ready(function(e) {
-	$(".clickableTreeArrow").mouseover(admin_mouseOver);
-	$(".clickableTreeArrow").mouseout(admin_mouseOut);
-	$(".clickableTreeArrow").click(admin_mouseClick);
-	$(".clickableTreeItem").click(admin_mouseClick);
+    $(".clickableTreeArrow").mouseover(admin_mouseOver);
+    $(".clickableTreeArrow").mouseout(admin_mouseOut);
+    $(".clickableTreeArrow").click(admin_mouseClick);
+    $(".clickableTreeItem").click(admin_mouseClick);
+
+    // Bit dirty, but cannot use the bind function,
+    // the JQuery event wrapper does not include the dataTransfer attribute
+    $(".contentroot").attr({
+        "ondrop":       "admin_upload(event);",
+        "ondragenter":  "admin_dragenter(event);",
+        "ondragover":   "admin_dragover(event);",
+        "ondragleave":  "admin_dragout(event);"
+    });
 });
