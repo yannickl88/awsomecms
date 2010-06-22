0bcf215cab5ececb6affb0c62b61ebf63768ec0d
yannick.l.88 <yannick.l.88@c9b2d25c-0a91-11df-a582-91aacf084772>
2010-06-22 21:29:13 +0000

    * updated passwordfield to have a salt value and sha1 instaid of md5

--- components/users/db/install.sql
+++ components/users/db/install.sql
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS `userdata` (
 CREATE TABLE IF NOT EXISTS `users` (
   `user_id` int(11) NOT NULL auto_increment,
   `user_name` varchar(100) NOT NULL,
-  `user_pass` varchar(100) NOT NULL,
+  `user_pass` varchar(150) NOT NULL,
   `user_privileges` bigint(20) NOT NULL,
   PRIMARY KEY  (`user_id`),
   UNIQUE KEY `user_name` (`user_name`)
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 
 INSERT INTO `users` (`user_name`, `user_pass`, `user_privileges`) VALUES
-('admin', 'fe01ce2a7fbac8fafaed7c982a04e229', 1);
+('admin', 'a:2:{s:8:"password";s:40:"f88430b2bd2ee105810d924f3ea47a39d09b3c75";s:4:"salt";s:9:"UDRaaEuSY";}', 1);
 
 INSERT INTO `auth` (`auth_component`, `auth_action`, `auth_privilage`) VALUES('users', 'action_add', 'admin');
 INSERT INTO `auth` (`auth_component`, `auth_action`, `auth_privilage`) VALUES('users', 'action_delete', 'admin');
