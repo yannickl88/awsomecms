dce1c683bcaa5342f07ac0ccafc47ee1cae6af50
yannick.l.88@gmail.com <yannick.l.88@gmail.com@c9b2d25c-0a91-11df-a582-91aacf084772>
2010-08-08 22:06:38 +0000

    * drag and drop HTML5 file upload now complies to the HTML5 specs. It will no longer work with firefox 3.6 or lower. But works in FireFox 4.01b * Added width and height to youtube BBCodeElement * BBCodeParser now also fixes HTML entities

--- trunk/components/page/install/js/admin.js
+++ trunk/components/page/install/js/admin.js
@@ -73,12 +73,6 @@ var Editor = function(element, options)
         this._addButton("Image", "[img]", "[/img]", 21);
         this._addButton("Youtube", "[youtube]", "[/youtube]", 30);
     };
-    /**
-     * Handel the resize for the editor
-     * NOTE: do not bind this, but pass the event
-     * 
-     * @param Event e
-     */
     this.resize = function(e) {
         console.log("RESIZE START");
         var editor = this; //local var for refering in listeners
@@ -866,183 +860,142 @@ var TreeAction = function(icon, url, root)
     
     this.init();
 }
-/**
- * TODO refactor in Object the functions:
- *   file_upload
- *   file_getType
- *   file_dragenter
- *   file_dragout
- *   file_bindListeners
- */
-function file_upload(event) {
-    var data = event.dataTransfer;
+var FileUpload = function(element) {
+    this.element = element;
     
-    if(data && data.files.length > 0)
-    {
-        var boundary = '------multipartformboundary' + (new Date).getTime();
-        var dashdash = '--';
-        var crlf     = '\r\n';
-    
-        /* For each dropped file. */
-        for (var i = 0; i < data.files.length; i++) 
+    this.init = function() {
+        this.bind();
+    };
+    this.getType = function(fileType) {
+        if(fileType.match(/^image\/.*$/i))
         {
-            /* Build RFC2388 string. */
-            var builder = '';
-            var file = data.files[i];
-            
-            //form data
-            builder += dashdash+boundary+crlf;
-            builder += 'Content-Disposition: form-data; name="component"';
-            builder += crlf+crlf;
-            builder += "file"
-            builder += crlf;
-            
-            builder += dashdash+boundary+crlf;
-            builder += 'Content-Disposition: form-data; name="action"';
-            builder += crlf+crlf;
-            builder += "upload"
-            builder += crlf;
-            
-            
-            /* Generate headers. */
-            builder += dashdash;
-            builder += boundary;
-            builder += crlf;
-            builder += 'Content-Disposition: form-data; name="file_data"';
-            if (file.fileName)
-            {
-              builder += '; filename="' + file.fileName + '"';
-            }
-            builder += crlf;
-    
-            builder += 'Content-Type: ';
-            if(file.type)
-            {
-                builder += file.type
-            }
-            else
-            {
-                builder += 'application/octet-stream'
-            }
-            builder += crlf;
-            builder += crlf; 
-    
-            /* Append binary data. */
-            builder += file.getAsBinary();
-            builder += crlf;
-            
-            /* Mark end of the request. */
-            builder += dashdash;
-            builder += boundary;
-            builder += dashdash;
-            builder += crlf;
-        
-            var xhr = new XMLHttpRequest();
-            xhr.open("POST", "/", true);
-            xhr.setRequestHeader('Content-Type', 'multipart/form-data; boundary=' + boundary);
-            xhr.setRequestHeader('X-File-Name', file.fileName);
-            xhr.setRequestHeader('X-File-Size', file.fileSize);
-            
-            //add Loader to the tree
-            var parent = uploadFolder.nodes[file_getType(file.type)];
-            var nodes = new TreeItem(file.name.substr(0, file.name.lastIndexOf(".")), parent, null, uploadFolder.root, "loader");
-            uploadFolder.root.addChild(nodes);
-            nodes.parent.open();
-            
-            //add the node to the loader
-            xhr.node = nodes;
-            
-            xhr.onload = function(event) { 
-                /* If we got an error display it. */
-                if (event.currentTarget.responseText)
-                {
-                    var data = $.parseJSON(event.currentTarget.responseText);
-                    if(data.error)
-                    {
-                        event.currentTarget.node.parent.removeChild(event.currentTarget.node);
-                    }
-                    else
-                    {
-                        event.currentTarget.node.icon.attr("src", event.currentTarget.node.root.getIcon(file_getType(data.type)));
-                        event.currentTarget.node.link.attr("href", data.editLink)
-                        event.currentTarget.node.addAction(new TreeAction("view", data.viewLink, event.currentTarget.node.root));
-                        event.currentTarget.node.addAction(new TreeAction("delete", data.deleteLink, event.currentTarget.node.root));
-                    }
-                }
-            };
-            
-            xhr.sendAsBinary(builder); //TODO This needs to be fixed when there is an update to the firefox File API
+            return "images";
         }
-    }
+        else if(fileType.match(/^audio\/.*$/i))
+        {
+            return "vid";
+        }
+        else if(fileType.match(/^video\/.*$/i))
+        {
+            return "vid";
+        }
+        else if(fileType.match(/^application\/vnd\.openxmlformats\-officedocument\.spreadsheetml\.sheet$/i))
+        {
+            return "docs";
+        }
+        else if(fileType.match(/^application\/vnd\.openxmlformats\-officedocument\.wordprocessingml\.document$/i))
+        {
+            return "docs";
+        }
+        else if(fileType.match(/^application\/vnd\.ms-powerpoint$/i))
+        {
+            return "docs";
+        }
+        else if(fileType.match(/^application\/msword$/i))
+        {
+            return "docs";
+        }
+        else if(fileType.match(/^application\/vnd\.oasis\..*$/i))
+        {
+            return "docs";
+        }
+        else 
+        {
+            return "other";
+        }
+    };
+    this.dragenter = function(e) {
+        $(e.currentTarget).addClass("fileFocus");
+        
+        e.stopPropagation();
+        e.preventDefault();
+    };
+    this.dragout = function(e) {
+        $(e.currentTarget).removeClass("fileFocus");
+        
+        e.stopPropagation();
+        e.preventDefault();
+    };
+    this.bind = function() {
+        var upload = this;
+        
+        //bind the listeners if we comply to the HTML5 spec
+        if(typeof FormData == 'function') {
+            this.element.addEventListener('drop', function(e) {
+                upload.upload(e);
+            }, false);
+            this.element.addEventListener('dragenter', function(e) {
+                upload.dragenter(e);
+            }, false);
+            this.element.addEventListener('dragover', function(e) {
+                upload.dragenter(e);
+            }, false);
+            this.element.addEventListener('dragout', function(e) {
+                upload.dragout(e);
+            }, false);
+            this.element.addEventListener('dragleave', function(e) {
+                upload.dragout(e);
+            }, false);
+        }
+    };
+    this.upload = function(e) {
+        var data = e.dataTransfer;
+        
+        if(data && data.files.length > 0)
+        {
+            /* For each dropped file. */
+            for ( var i = 0; i < data.files.length; i++) {
+                var file = data.files[i];
+                
+                var formData = new FormData();
+                formData.append("component", "file");
+                formData.append("action", "upload");
+                formData.append("file_data", file);
+                
+                //add Loader to the tree
+                var treeNode = this.element.object;
+                var parent = treeNode.nodes[this.getType(file.type)];
+                var loaderNode = new TreeItem(file.name.substr(0, file.name.lastIndexOf(".")), parent, null, treeNode.root, "loader");
+                
+                treeNode.root.addChild(loaderNode);
+                loaderNode.parent.open();
+                
+                // create a new xhr object
+                var xhr = new XMLHttpRequest();
+                // add listeners
+                var u = this;
+                xhr.onload = function(event) {
+                    /* If we got an error display it. */
+                    if (event.currentTarget.responseText)
+                    {
+                        var data = $.parseJSON(event.currentTarget.responseText);
+                        
+                        if(data.error)
+                        {
+                            loaderNode.parent.removeChild(loaderNode);
+                        }
+                        else
+                        {
+                            loaderNode.icon.attr("src", loaderNode.root.getIcon(u.getType(data.type)));
+                            loaderNode.link.attr("href", data.editLink)
+                            loaderNode.addAction(new TreeAction("view", data.viewLink, loaderNode.root));
+                            loaderNode.addAction(new TreeAction("delete", data.deleteLink, loaderNode.root));
+                        }
+                    }
+                };
+
+                xhr.open("POST", "/");
+                xhr.send(formData);
+            }
+        }
+        
+        this.dragout(e);
+        
+        e.stopPropagation();
+        e.preventDefault();
+    };
     
-    file_dragout(event);
-    
-    event.stopPropagation();
-    event.preventDefault();
-}
-function file_getType(fileType)
-{
-    if(fileType.match(/^image\/.*$/i))
-    {
-        return "images";
-    }
-    else if(fileType.match(/^audio\/.*$/i))
-    {
-        return "vid";
-    }
-    else if(fileType.match(/^video\/.*$/i))
-    {
-        return "vid";
-    }
-    else if(fileType.match(/^application\/vnd\.openxmlformats\-officedocument\.spreadsheetml\.sheet$/i))
-    {
-        return "docs";
-    }
-    else if(fileType.match(/^application\/vnd\.openxmlformats\-officedocument\.wordprocessingml\.document$/i))
-    {
-        return "docs";
-    }
-    else if(fileType.match(/^application\/vnd\.ms-powerpoint$/i))
-    {
-        return "docs";
-    }
-    else if(fileType.match(/^application\/msword$/i))
-    {
-        return "docs";
-    }
-    else if(fileType.match(/^application\/vnd\.oasis\..*$/i))
-    {
-        return "docs";
-    }
-    else 
-    {
-        return "other";
-    }
-}
-function file_dragenter(event)
-{
-    $(event.currentTarget).addClass("fileFocus");
-    
-    event.stopPropagation();
-    event.preventDefault();
-}
-function file_dragout(event)
-{
-    $(event.currentTarget).removeClass("fileFocus");
-    
-    event.stopPropagation();
-    event.preventDefault();
-}
-function file_bindListeners(element)
-{
-    if(XMLHttpRequest.prototype.sendAsBinary) //check if we can actully upload (Firefox 3.6+ only)
-    {
-        element.addEventListener('drop', file_upload, false);
-        element.addEventListener('dragenter', file_dragenter, false);
-        element.addEventListener('dragover', file_dragenter, false);
-        element.addEventListener('dragout', file_dragout, false);
-        element.addEventListener('dragleave', file_dragout, false);
-    }
+    this.init(); //call constructor
 }
 
 /**
@@ -1111,20 +1064,22 @@ var TagField = function(element, tags)
      */
     this.init = function(element, tags)
     {
-        this.element = element;
-        this.element.attr("autocomplete", "off");
-        var pos = element.position();
-        this.tags = tags.sort();
-        this.panel = $("<ul class='tagPanel'></ul>");
-        this.panel.css({left: pos.left, top: pos.top + this.element.outerHeight(), display: "none", width: this.element.innerWidth()});
-        
-        $("body").append(this.panel);
-        
-        this.element.focus(this.handelFocus);
-        this.element.blur(this.handelBlur);
-        this.element.keyup(this.handelKey);
-        this.element.keydown(this.handelKeyDown);
-        this.element.get(0).field = this;
+        if(element.length > 0) {
+            this.element = element;
+            this.element.attr("autocomplete", "off");
+            var pos = element.position();
+            this.tags = tags.sort();
+            this.panel = $("<ul class='tagPanel'></ul>");
+            this.panel.css({left: pos.left, top: pos.top + this.element.outerHeight(), display: "none", width: this.element.innerWidth()});
+            
+            $("body").append(this.panel);
+            
+            this.element.focus(this.handelFocus);
+            this.element.blur(this.handelBlur);
+            this.element.keyup(this.handelKey);
+            this.element.keydown(this.handelKeyDown);
+            this.element.get(0).field = this;
+        }
     };
     /**
      * Handel when the field get focus
