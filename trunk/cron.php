0f94fad124b8740ef8ab5c3ccda1f9a89a6f4f8e
yannick.l.88@gmail.com <yannick.l.88@gmail.com@c9b2d25c-0a91-11df-a582-91aacf084772>
2010-08-24 20:35:36 +0000

    * added cron job for the files component * rewritten cron script to use the CLI framework

--- trunk/cron.php
+++ trunk/cron.php
@@ -10,42 +10,97 @@
  *
  * @version $Revision$
  */
-global $websiteroot, $start;
+global $websiteroot;
+$websiteroot = dirname(__FILE__).DIRECTORY_SEPARATOR."htdocs";
 
-$start = microtime(true);
-$websiteroot = dirname(__FILE__)."/htdocs";
+require_once 'core/init.inc';
 
-require_once $websiteroot.'/../core/init.inc';
-require_once $websiteroot.'/../core/class.Cache.inc';
-require_once $websiteroot.'/../libs/class.CronParser.inc';
+import('libs/class.CronParser.inc');
+import('libs/class.CLI.inc');
 
-Debugger::getInstance()->setType(Debugger::CLI);
-$components = RegisterManager::getInstance()->getComponents();
-
-$now = $lastrun = time();
-if(Cache::has("cron"))
+class Usage extends CLIAction
 {
-    $lastrun = Cache::get("cron");
-}
-$cronParser = new CronParser();
-
-foreach($components as $component)
-{
-    $cronTime = Config::get($component->component_name, false, "cron");
-    
-    $count = $lastrun;
-    if($cronTime)
+    /**
+     * (non-PHPdoc)
+     * @see libs/CLIAction::exec()
+     *
+     * @param CLI $cli
+     * @param String $action
+     */
+    public function exec($cli, $action)
     {
-        while($count <= $now)
+        $cli->output("php -f cron.php [action] [-t=time]");
+        $cli->output("");
+        $cli->output("Options:");
+        $cli->output("action       help, run");
+        $cli->output("  help       This text");
+        $cli->output("  run        Run the cron jobs");
+        $cli->output("time         Specify a time to which is now, used for debugging.");
+        $cli->output("             See strtotime for time formats.");
+        $cli->output("");
+        $cli->output("Usage:");
+        $cli->output("php -f cron.php run");
+        $cli->output("             Run the con jobs");
+        $cli->output("php -f cron.php run -t=12:00");
+        $cli->output("             Run the con jobs using 12:00 today as now");
+        $cli->output("");
+    }
+}
+class Cron extends CLIAction
+{
+    /**
+     * (non-PHPdoc)
+     * @see libs/CLIAction::exec()
+     *
+     * @param CLI $cli
+     * @param String $action
+     */
+    public function exec($cli, $action)
+    {
+        $now = $lastrun = time();
+        $cache = true;
+        if(Cache::has("cron"))
         {
-            if($cronParser->mustRunNow($cronTime, $count))
-            {
-                Component::init($component->component_name)->execCron();
-            }
-            
-            $count += 60;
+            $lastrun = Cache::get("cron");
         }
+        if($cli->getArg("-t") !== false) //check if we got an time value
+        {
+            $lastrun = $now = strtotime($cli->getArg("-t"));
+            $cache = false;
+        }
+        $cronParser = new CronParser();
+        $components = RegisterManager::getInstance()->getComponents();
+        
+        foreach($components as $component)
+        {
+            $cronTime = Config::get($component->component_name, false, "cron");
+            
+            $count = $lastrun;
+            if($cronTime)
+            {
+                while($count <= $now)
+                {
+                    if($cronParser->mustRunNow($cronTime, $count))
+                    {
+                        Component::init($component->component_name)->execCron();
+                    }
+                    
+                    $count += 60;
+                }
+            }
+        }
+        if($cache) Cache::set("cron", $now);
     }
 }
 
-Cache::set("cron", $now);
+
+$cli = new CLI($argv);
+$cli->setVerbose();
+$cli->header();
+
+//register the actions
+$cli->registerAction("help",        "Usage");
+$cli->registerAction("run",         "Cron");
+
+if($cli->getAction() == "") $cli->setAction("run");
+$cli->doAction();
